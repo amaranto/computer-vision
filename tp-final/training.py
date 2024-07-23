7ef5464a227a236729040a6fb683cbfd6148b598
Ezequiel Arevalo <eze.m.arevalo@gmail.com>
2024-07-23 14:30:04 -0300

    .

--- tp-final/training.py
+++ tp-final/training.py
@@ -4,12 +4,13 @@ import torch
 
 if __name__=='__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    #model = YOLO("../runs/detect/train3/weights/best.pt")
-    model = YOLO("yolov8m.yaml")
+    model = YOLO("../runs/detect/train/weights/best.pt")
+    #model = YOLO("yolov8m.yaml")
     results = model.train(
         data="./data.yaml",  
         save=True, 
         #workers=2, 
+        resume=True,
         dropout=0.1,
         val=True,
         plots=True,
@@ -35,18 +36,18 @@ if __name__=='__main__':
     
     success = model.export(path="yolov8mTruco.pt")
 
-    # pictures = glob.glob("./datasets/custom/*")
-    # print (pictures)
+    pictures = glob.glob("./datasets/custom/*")
+    print (pictures)
     
-    # for picture in pictures:
-    #     filename = picture.split("/")[-1]
-    #     results = model(picture)  
-    #     for result in results:
-    #         boxes = result.boxes
-    #         masks = result.masks
-    #         keypoints = result.keypoints
-    #         probs = result.probs
-    #         obb = result.obb
-    #         result.show()
-    #         result.save(filename=f"./datasets/results/{filename}")
+    for picture in pictures:
+        filename = picture.split("/")[-1]
+        results = model(picture)  
+        for result in results:
+            boxes = result.boxes
+            masks = result.masks
+            keypoints = result.keypoints
+            probs = result.probs
+            obb = result.obb
+            result.show()
+            result.save(filename=f"./datasets/results/{filename}")
         
