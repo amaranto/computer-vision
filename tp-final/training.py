6f305a7777f5bca0ddd83fd3d8a5b3bddeaaa0bc
Ezequiel Arevalo <earevalo@11solutions.cl>
2024-07-27 19:37:09 +0000

    .

--- tp-final/training.py
+++ tp-final/training.py
@@ -4,37 +4,42 @@ import torch
 
 if __name__=='__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = YOLO("../runs/detect/train/weights/best.pt")
-    #model = YOLO("yolov8m.yaml")
+    #model = YOLO("../runs/detect/train/weights/best.pt")
+    model = YOLO("yolov8m.yaml")
     results = model.train(
-        data="./data.yaml",  
-        save=True, 
-        #workers=2, 
-        resume=True,
-        dropout=0.1,
-        val=True,
-        plots=True,
-        device=device,
-        imgsz = 640,
-        save_period=10,
-        # dfl = 4.5,
-        # box = 7.5,
-        cls = 0.7,
-        epochs=50,  
-        augment=True,
-        perspective = 0.0,
-        scale = 0.1,
-        mixup = 0.0,
-        mosaic = 0.0,
-        shear = 0.0,
-        degrees = 0.3,
-        bgr = 0.0,
-        copy_paste = 0.0
-    )
+         data="./data.yaml",  
+         save=True, 
+         batch=0.8,
+         cls=0.8,
+         patience=20,
+         #workers=2, 
+         resume=False,
+         dropout=0.1,
+         val=True,
+         plots=True,
+         device=device,
+         imgsz = 640,
+         save_period=10,
+         # dfl = 4.5,
+         # box = 7.5,
+         epochs=100,  
+         hsv_s=0.7,
+         hsv_v=0.4,
+         hsv_h=0.2,
+         augment=True,
+         perspective = 0.001,
+         scale = 0.1,
+         mixup = 0.0,
+         mosaic = 1.0,
+         shear = 0.0,
+         degrees = 0.3,
+         bgr = 0.0,
+         copy_paste = 1.0
+     )
 
     results = model.val()
     
-    success = model.export(path="yolov8mTruco.pt")
+#    success = model.export()
 
     pictures = glob.glob("./datasets/custom/*")
     print (pictures)
